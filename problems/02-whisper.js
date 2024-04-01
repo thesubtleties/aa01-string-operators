bd28805e729846178492dd72634cc0bea0c42a02
Steven <stevenglab@gmail.com>
2024-04-01 16:37:38 -0700

    finished problems

---
+++ problems/02-whisper.js
@@ -0,0 +1,15 @@
+/*
+Define a function whisper that takes in a string parameter and returns a
+"whispered" version of that string.
+*/
+
+// Your code here 
+
+function whisper(str) {
+    return `...${str.toLowerCase()}...`
+}
+// console.log(whisper("Hey Buddy")); // "...hey buddy..."
+// console.log(whisper("YEA! that was FUN")); // "...yea! that was fun..."
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+module.exports = whisper;
